5a04e844f0b8817fb9513acbfe2a5942018f091e
Jun Ji <junji64@gmail.com>
2018-05-10 13:06:54 +0900

    Create olympic_flag.py

---
+++ olympic_flag.py
@@ -0,0 +1,9 @@
+def draw_olympic_symbol():
+	positions = [[0,0,"green"], [-120,0,"yellow"], [60,60,"red"], [-60,60,"black"], [-180,60,"blue"]]
+	t.pensize(8)
+	for x,y,c in positions:
+		t.penup()
+		t.goto(x,y)
+		t.pendown()
+		t.color(c,c)
+		t.circle(55)
