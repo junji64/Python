d1682a4dcbd8f3919015274b0641eabce8f0a9fb
Jun Ji <junji64@gmail.com>
2018-05-10 13:14:58 +0900

    Create ch9_dict.py

---
+++ ch9_dict.py
@@ -0,0 +1,14 @@
+english_dict = dict()
+
+english_dict['one'] = '하나'
+english_dict['two'] = '둘'
+english_dict['three'] = '셋'
+english_dict['four'] = '넷'
+english_dict['five'] = '다섯'
+english_dict['six'] = '여섯'
+english_dict['seven'] = '일곱'
+english_dict['eight'] = '여덟'
+english_dict['nine'] = '아홉'
+
+word = input("단어를 입력하시오: ");
+print (english_dict[word])
