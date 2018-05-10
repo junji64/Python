08598646aba69ea061d774e0decdc21dbf114de0
Jun Ji <junji64@gmail.com>
2018-05-10 13:16:06 +0900

    Update olympic_flag.py

--- olympic_flag.py
+++ olympic_flag.py
@@ -1,3 +1,5 @@
+import turtle
+
 def draw_olympic_symbol():
 	positions = [[0,0,"green"], [-120,0,"yellow"], [60,60,"red"], [-60,60,"black"], [-180,60,"blue"]]
 	t.pensize(8)
@@ -7,3 +9,6 @@ def draw_olympic_symbol():
 		t.pendown()
 		t.color(c,c)
 		t.circle(55)
+
+t = turtle.Turlte()
+draw_olympic_symbol()
